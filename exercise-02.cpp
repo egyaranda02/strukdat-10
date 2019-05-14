16b641f209b8a26d2bdecda213d9118db768b40f
egyaranda02 <47957667+egyaranda02@users.noreply.github.com>
2019-05-14 19:09:36 +0700

    Update exercise-02.cpp

--- exercise-02.cpp
+++ exercise-02.cpp
@@ -22,7 +22,7 @@ Queue Q;
 void createList(Queue& Q)
 {
 	Q.Head=NULL;
-    Q.Tail=NULL;
+   	Q.Tail=NULL;
 }
 
 void createElement(pointer& pBaru)
@@ -36,10 +36,10 @@ void cetak(Queue Q)
 {
     pointer pBantu=Q.Head;
     if(Q.Head==NULL && Q.Tail==NULL)
-	{
+    {
         cout<<"Kosong"<<endl;
     }
-	else
+else
 	{
         while(pBantu!=NULL)
 		{
@@ -58,7 +58,7 @@ void insertQueue(Queue& Q,pointer pBaru)
         Q.Tail=pBaru;
     }
     else 
-	{ 
+    { 
         Q.Tail->next=pBaru;
         Q.Tail=pBaru;
     }
@@ -67,19 +67,19 @@ void insertQueue(Queue& Q,pointer pBaru)
 void deleteQueue(Queue& Q, pointer& pHapus)
 {
     if(Q.Head==NULL && Q.Tail==NULL) 
-	{ 
+    { 
         pHapus=NULL;
         cout<<"List Queue kosong"<<endl;
     }
     else if(Q.Head->next==NULL) 
-	{
+    {
         pHapus=Q.Head;
         cout<<"Data yang dihapus adalah	: "<<pHapus->info<<endl;
         Q.Head=NULL;
         Q.Tail=NULL;
     }
     else 
-	{ 
+    { 
         pHapus=Q.Head;
         cout<<"Data yang dihapus adalah	: "<<pHapus->info<<endl;
         Q.Head=Q.Head->next;
@@ -103,8 +103,8 @@ int main()
     cout<<endl;
     cout<<"Pop : "<<endl;
     for(int i=0;i<n;i++)
-	{
-		deleteQueue(Q,pHapus);
+    {
+	deleteQueue(Q,pHapus);
     }
 }
 
